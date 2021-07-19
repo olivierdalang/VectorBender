817b7bd3c3efe7ad3b4eb9760b76e8b2adcfe1bf
Nath <nathanlloyd@protonmail.com>
2021-07-20 00:23:53 +0100

    Update vectorbender.py
    I have run NO tests.  improved variable naming, changed iterative step to use recursion to reconstruct lists with transform, instead of having varying but repetitive code.  Simplified feature counting as a separate function and reduced if statements where any count < 4 just returns it's feature count and anything above 3 considered it's dependency status.
    
    `unload` could probably be improved if dialog is a sub classed from `_io` module and just check that the file is closed, assigning None to it is a bit pointless, removed `u` prefix from strings as that is an old Unicode thing from python 2.  Python 3 supports Unicode full stop

--- vectorbender.py
+++ vectorbender.py
@@ -20,17 +20,15 @@
  ***************************************************************************/
 """
 
-# Import the PyQt and QGIS libraries
+# Import the and QGIS graphical libraries
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtWidgets import *
 from qgis.PyQt.QtGui import *
 from qgis.core import *
 from qgis.gui import *
 
-# Basic dependencies
-import os.path
-import sys
-import math
+# standard library dependency
+from os.path import join, dirname
 
 # More tricky dependencies
 from distutils.version import StrictVersion
@@ -39,41 +37,42 @@ try:
     import matplotlib.tri
     minVersion = '1.3.0'
     if StrictVersion(matplotlib.__version__) < StrictVersion(minVersion):
-        dependenciesStatus=1
-        QgsMessageLog.logMessage("Matplotlib version too old (%s instead of %s). You won't be able to use the bending algorithm" % (matplotlib.__version__,minVersion), 'VectorBender')
-except Exception:
-    QgsMessageLog.logMessage("Matplotlib is missing. You won't be able to use the bending algorithm", 'VectorBender')
+        dependenciesStatus = 1
+        QgsMessageLog.logMessage(
+            f"Matplotlib version too old ({matplotlib.__version__} instead of {minVersion}). You won't be able to use the bending algorithm",
+            "VectorBender")
+except Exception: # <---- what type of exception??
+    QgsMessageLog.logMessage(
+        "Matplotlib is missing. You won't be able to use the bending algorithm", "VectorBender")
     dependenciesStatus = 0
 
-# Other classes
+# Other modules
 from .vectorbendertransformers import *
 from .vectorbenderdialog import VectorBenderDialog
 from .vectorbenderhelp import VectorBenderHelp
 
-class VectorBender:
 
+class VectorBender:
     def __init__(self, iface):
         self.iface = iface
-        self.dlg = VectorBenderDialog(iface,self)
-
-        self.ptsA = []
-        self.ptsB = []
-
+        self.dialog = VectorBenderDialog(iface, self)
+        self.ptsA = list()
+        self.ptsB = list()
         self.transformer = None
-
         self.aboutWindow = None
 
-
     def initGui(self):
-
-        self.action = QAction( QIcon(os.path.join(os.path.dirname(__file__),'resources','icon.png')), "Vector Bender", self.iface.mainWindow())
+        self.action = QAction(
+            QIcon(join(dirname(__file__), "resources", "icon.png")),
+            "Vector Bender", self.iface.mainWindow())
         self.action.triggered.connect(self.showUi)
         self.iface.addToolBarIcon(self.action)
-        self.iface.addPluginToMenu(u"&Vector Bender", self.action)
-
-        self.helpAction = QAction( QIcon(os.path.join(os.path.dirname(__file__),'resources','about.png')), "Vector Bender Help", self.iface.mainWindow())
+        self.iface.addPluginToMenu("&Vector Bender", self.action)
+        self.helpAction = QAction(
+            QIcon(join(dirname(__file__), "resources", "about.png")),
+            "Vector Bender Help", self.iface.mainWindow())
         self.helpAction.triggered.connect(self.showHelp)
-        self.iface.addPluginToMenu(u"&Vector Bender", self.helpAction)
+        self.iface.addPluginToMenu("&Vector Bender", self.helpAction)
 
     def showHelp(self):
         if self.aboutWindow is None:
@@ -82,22 +81,30 @@ class VectorBender:
         self.aboutWindow.raise_()
 
     def unload(self):
-        if self.dlg is not None:
-            self.dlg.close()
-            self.dlg = None
-
+        # coudn't you use context syntax or is thsi a subclass from _io
+        # so we can use `if self.dialog.closed:`
+        if self.dialog is not None:
+            self.dialog.close()
+            self.dialog = None
         if self.aboutWindow is not None:
             self.aboutWindow.close()
             self.aboutWindow = None
-
-        self.iface.removePluginMenu(u"&Vector Bender", self.action)
-        self.iface.removePluginMenu(u"&Vector Bender", self.helpAction)
+        self.iface.removePluginMenu("&Vector Bender", self.action)
+        self.iface.removePluginMenu("&Vector Bender", self.helpAction)
         self.iface.removeToolBarIcon(self.action)
 
     def showUi(self):
-        self.dlg.show()
-        self.dlg.raise_()
-        self.dlg.refreshStates()
+        self.dialog.show()
+        self.dialog.raise_()
+        self.dialog.refreshStates()
+    
+    def featureCount(self, length):
+        global dependenciesStatus
+        if length <= 3: return length
+        elif length >= 4:
+            if dependenciesStatus != 2: return 5
+            else: return 4
+        return 0
 
     def determineTransformationType(self):
         """Returns :
@@ -106,171 +113,115 @@ class VectorBender:
             2 if two pairs found => linear
             3 if three pairs found => affine
             4 if four or more pairs found => bending"""
-
-        pairsLayer = self.dlg.pairsLayer()
-
+        pairsLayer = self.dialog.pairsLayer()
         if pairsLayer is None:
             return 0
-
-        featuresCount = len(pairsLayer.selectedFeatureIds()) if self.dlg.restrictBox_pairsLayer.isChecked() else len(pairsLayer.allFeatureIds())
-
-        if featuresCount == 1:
-            return 1
-        elif featuresCount == 2:
-            return 2
-        elif featuresCount == 3:
-            return 3
-        elif featuresCount >= 4:
-            if dependenciesStatus != 2:
-                return 5
+        elif self.dialog.restrictBox_pairsLayer.isChecked():
+            self.featuresCount(len(pairsLayer.selectedFeatureIds()))
+        else:
+            self.featuresCount(len(pairsLayer.allFeatureIds())
+    
+    def delaunay(self, pairsLayer, transType):
+        # Loading the delaunay
+        restrictToSelection = self.dialog.restrictBox_pairsLayer.isChecked()
+        if transType == 4:
+            self.dialog.displayMsg(f"Loading delaunay mesh ({len(self.ptsA)} points) ...")
+            QCoreApplication.processEvents()
+            self.transformer = BendTransformer(pairsLayer, restrictToSelection, self.dialog.bufferValue())
+        elif transType == 3:
+            self.dialog.displayMsg("Loading affine transformation vectors...")
+            self.transformer = AffineTransformer(pairsLayer, restrictToSelection)
+        elif transType == 2:
+            self.dialog.displayMsg("Loading linear transformation vectors...")
+            self.transformer = LinearTransformer(pairsLayer, restrictToSelection)
+        elif transType == 1:
+            self.dialog.displayMsg("Loading translation vector...")
+            self.transformer = TranslationTransformer(pairsLayer, restrictToSelection)
+        else:
+            self.dialog.displayMsg("INVALID TRANSFORMATION TYPE - YOU SHOULDN'T HAVE BEEN ABLE TO HIT RUN")
+            return False
+        return True
+    
+    def pairsToPinsTransformation(self, pairsLayer):
+        if self.dialog.pairsToPinsCheckBox.isChecked():
+            if not self.dialog.restrictBox_pairsLayer.isChecked():
+                features = pairsLayer.getFeatures()
+                total = pairsLayer.featureCount()
             else:
-                return 4
-
-        return 0
+                features = pairsLayer.selectedFeatures()
+                total = len(features)
+            self.dialog.progressBar.setValue(0)
+            self.dialog.displayMsg(
+                f"Starting to transform {total} pairs to pins . . . ")
+            QCoreApplication.processEvents()
+            pairsLayer.beginEditCommand("Transforming pairs to pins")
+            for count, feature in enumerate(features):
+                self.dialog.progressBar.setValue((count / total) * 100)
+                self.dialog.displayMsg(
+                    f"Transforming pair to pin {count} out of {total} ... {count/total:%}")
+                QCoreApplication.processEvents()
+                geom = feature.geometry().asPolyline()
+                pairsLayer.changeGeometry(
+                    feature.id(), QgsGeometry.fromPolylineXY([geom[-1], geom[-1]]))
+            pairsLayer.endEditCommand()
+    
+    def recursiveTransform(self, point_list):
+        if not isinstance(point_list, list):
+            return self.transformer.map(point_list)
+        else:
+            return [self.recursiveTransform(point) for point in point_list]
 
     def run(self):
-
-        self.dlg.progressBar.setValue( 0 )
-
-        toBendLayer = self.dlg.toBendLayer()
-        pairsLayer = self.dlg.pairsLayer()
-
-        transType = self.determineTransformationType()
-
-        # Loading the delaunay
-        restrictToSelection = self.dlg.restrictBox_pairsLayer.isChecked()
-        if transType==4:
-            self.dlg.displayMsg( "Loading delaunay mesh (%i points) ..." % len(self.ptsA) )
-            QCoreApplication.processEvents()
-            self.transformer = BendTransformer( pairsLayer, restrictToSelection, self.dlg.bufferValue() )
-        elif transType==3:
-            self.dlg.displayMsg( "Loading affine transformation vectors..."  )
-            self.transformer = AffineTransformer( pairsLayer, restrictToSelection )
-        elif transType==2:
-            self.dlg.displayMsg( "Loading linear transformation vectors..."  )
-            self.transformer = LinearTransformer( pairsLayer, restrictToSelection )
-        elif transType==1:
-            self.dlg.displayMsg( "Loading translation vector..."  )
-            self.transformer = TranslationTransformer( pairsLayer, restrictToSelection )
-        else:
-            self.dlg.displayMsg( "INVALID TRANSFORMATION TYPE - YOU SHOULDN'T HAVE BEEN ABLE TO HIT RUN" )
+        self.dialog.progressBar.setValue(0)
+        toBendLayer = self.dialog.toBendLayer()
+        pairsLayer = self.dialog.pairsLayer()
+        if not self.delaunay(pairsLayer, self.determineTransformationType()):
             return
-
         # Starting to iterate
-        features = toBendLayer.getFeatures() if not self.dlg.restrictBox_toBendLayer.isChecked() else toBendLayer.selectedFeatures()
-
-        count = toBendLayer.featureCount() if not self.dlg.restrictBox_toBendLayer.isChecked() else len(features)
-        self.dlg.displayMsg( "Starting to iterate through %i features..." % count )
+        if not self.dialog.restrictBox_toBendLayer.isChecked():
+            features = toBendLayer.getFeatures()
+            total = toBendLayer.featureCount()
+        else:
+            features = toBendLayer.selectedFeatures()
+            total = len(features)
+        self.dialog.displayMsg(f"Starting to iterate through {total} features...")
         QCoreApplication.processEvents()
-
         toBendLayer.beginEditCommand("Feature bending")
-        for i,feature in enumerate(features):
-
-            self.dlg.progressBar.setValue( int(100.0*float(i)/float(count)) )
-            self.dlg.displayMsg( "Aligning features %i out of %i..."  % (i, count))
+        for count, feature in enumerate(features):
+            self.dialog.progressBar.setValue((count / total) * 100)
+            self.dialog.displayMsg(
+                f"Aligning features {count} out of {total} ... {count/total:%}")
             QCoreApplication.processEvents()
-
             geom = feature.geometry()
-
-            #TODO : this cood be much simple if we could iterate through to vertices and use QgsGeometry.moveVertex(x,y,index), but QgsGeometry.vertexAt(index) doesn't tell wether the index exists, so there's no clean way to iterate...
-
+            # TODO : this code be much simple if we could iterate through to vertices
+            # and use QgsGeometry.moveVertex(x,y,index), but QgsGeometry.vertexAt(index)
+            # doesn't tell wether the index exists, so there's no clean way to iterate...
             if geom.type() == QgsWkbTypes.PointGeometry:
-
-                if not geom.isMultipart():
-                    # SINGLE PART POINT
-                    p = geom.asPoint()
-                    newGeom = QgsGeometry.fromPointXY( self.transformer.map(p) )
-
-                else:
-                    # MULTI PART POINT
-                    listA = geom.asMultiPoint()
-                    newListA = []
-                    for p in listA:
-                        newListA.append( self.transformer.map(p) )
-                    newGeom = QgsGeometry.fromMultiPointXY( newListA )
-
-            elif geom.type() == QgsWkbTypes.LineGeometry:
-
-                if not geom.isMultipart():
-                    # SINGLE PART LINESTRING
-                    listA = geom.asPolyline()
-                    newListA = []
-                    for p in listA:
-                        newListA.append( self.transformer.map(p) )
-                    newGeom = QgsGeometry.fromPolylineXY( newListA )
-
-                else:
-                    # MULTI PART LINESTRING
-                    listA = geom.asMultiPolyline()
-                    newListA = []
-                    for listB in listA:
-                        newListB = []
-                        for p in listB:
-                            newListB.append( self.transformer.map(p) )
-                        newListA.append( newListB )
-                    newGeom = QgsGeometry.fromMultiPolylineXY( newListA )
-
-            elif geom.type() == QgsWkbTypes.PolygonGeometry:
-
-                if not geom.isMultipart():
-                    # SINGLE PART POLYGON
-                    listA = geom.asPolygon()
-                    newListA = []
-                    for listB in listA:
-                        newListB = []
-                        for p in listB:
-                            newListB.append( self.transformer.map(p) )
-                        newListA.append( newListB )
-                    newGeom = QgsGeometry.fromPolygonXY( newListA )
-
-                else:
-                    # MULTI PART POLYGON
-                    listA = geom.asMultiPolygon()
-                    newListA = []
-                    for listB in listA:
-                        newListB = []
-                        for listC in listB:
-                            newListC = []
-                            for p in listC:
-                                newListC.append( self.transformer.map(p) )
-                            newListB.append( newListC )
-                        newListA.append( newListB )
-                    newGeom = QgsGeometry.fromMultiPolygonXY( newListA )
-
-            else:
-                # FALLBACK, JUST IN CASE ;)
-                newGeom = geom
-
-            toBendLayer.changeGeometry( feature.id(), newGeom )
-
+                if not geom.isMultipart(): # SINGLE PART POINT
+                    geom = QgsGeometry.fromPointXY(
+                        self.transformer.map(geom.asPoint()))
+                else: # MULTI PART POINT
+                    geom = QgsGeometry.fromMultiPointXY(
+                        self.recursiveTransform(geom.asMultiPoint()))
+            elif geom.type() == QgsWkbTypes.LineGeometry: # isinstance ?
+                if not geom.isMultipart():  # SINGLE PART LINESTRING
+                    geom = QgsGeometry.fromPolylineXY(
+                        self.recursiveTransform(geom.asPolyline()))
+                else: # MULTI PART LINESTRING
+                    geom = QgsGeometry.fromMultiPolylineXY(
+                        self.recursiveTransform(geom.asMultiPolyline()))
+            elif geom.type() == QgsWkbTypes.PolygonGeometry: # isinstance ?
+                if not geom.isMultipart(): # SINGLE PART POLYGON
+                    geom = QgsGeometry.fromPolygonXY(
+                        self.recursiveTransform(geom.asPolygon()))
+                else: # MULTI PART POLYGON
+                    geom = QgsGeometry.fromMultiPolygonXY(
+                        self.recursiveTransform(geom.asMultiPolygon()))
+            toBendLayer.changeGeometry(feature.id(), geom)
         toBendLayer.endEditCommand()
         toBendLayer.repaintRequested.emit()
-
-
         #Transforming pairs to pins
-        if self.dlg.pairsToPinsCheckBox.isChecked():
-
-            features = pairsLayer.getFeatures() if not self.dlg.restrictBox_pairsLayer.isChecked() else pairsLayer.selectedFeatures()
-
-            count = pairsLayer.featureCount() if not self.dlg.restrictBox_pairsLayer.isChecked() else len(features)
-            self.dlg.progressBar.setValue( 0 )
-            self.dlg.displayMsg( "Starting to transform %i pairs to pins..." % count )
-            QCoreApplication.processEvents()
-
-            pairsLayer.beginEditCommand("Transforming pairs to pins")
-            for i,feature in enumerate(features):
-
-                self.dlg.progressBar.setValue( int(100.0*float(i)/float(count)) )
-                self.dlg.displayMsg( "Transforming pair to pin %i out of %i..."  % (i, count))
-                QCoreApplication.processEvents()
-
-                geom = feature.geometry().asPolyline()
-
-                newGeom = QgsGeometry.fromPolylineXY( [geom[-1],geom[-1]] )
-                pairsLayer.changeGeometry( feature.id(), newGeom )
-
-            pairsLayer.endEditCommand()
-
-        self.dlg.displayMsg( "Finished !" )
-        self.dlg.progressBar.setValue( 100 )
+        self.pairsToPinsTransformation(pairsLayer)
+        self.dialog.displayMsg("Finished !")
+        self.dialog.progressBar.setValue(100)
         pairsLayer.repaintRequested.emit()
