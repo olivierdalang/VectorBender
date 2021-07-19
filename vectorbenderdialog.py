bab01d3cbae1bb453d606b13e05bcc1933ff5585
Nath <nathanlloyd@protonmail.com>
2021-07-19 22:56:22 +0100

    Update vectorbenderdialog.py
    Modified script without running any tests, improved variable naming, and utilized loops for repetitive/sequential code.  add line spaces between methods for better readability.
    
    Notability improved `createMemoryLayer` by changing suffix to integer which is incremented in while loop without condition.  Then utilized `f` literal strings for spacing in output.  Changed any formatted strings to `f` literals.  Lastly modified `checkRequirements` so uses `if/elif/else` logic with needing return in each conditional snippet

--- vectorbenderdialog.py
+++ vectorbenderdialog.py
@@ -7,15 +7,14 @@ from qgis.PyQt.QtGui import *
 from qgis.core import *
 from qgis.gui import *
 
-import os.path
+from os.path import join, dirname
 
 from .vectorbendertransformers import *
-
 
 class VectorBenderDialog(QtWidgets.QDialog):
     def __init__(self, iface, vb):
         QtWidgets.QDialog.__init__(self)
-        uic.loadUi(os.path.join(os.path.dirname(__file__),'ui_main.ui'), self)
+        uic.loadUi(join(dirname(__file__), "ui_main.ui"), self)
         self.setFocusPolicy(Qt.ClickFocus)
         #self.setWindowModality( Qt.ApplicationModal )
 
@@ -39,14 +38,14 @@ class VectorBenderDialog(QtWidgets.QDialog):
         # When those are changed, we recheck the requirements
         self.editModeButton_pairsLayer.clicked.connect(self.checkRequirements)
         self.editModeButton_toBendLayer.clicked.connect(self.checkRequirements)
-        self.comboBox_toBendLayer.activated.connect( self.checkRequirements )
-        self.pairsToPinsCheckBox.clicked.connect( self.checkRequirements )
+        self.comboBox_toBendLayer.activated.connect(self.checkRequirements)
+        self.pairsToPinsCheckBox.clicked.connect(self.checkRequirements)
 
         # When those are changed, we change the transformation type (which also checks the requirements)
-        self.comboBox_toBendLayer.activated.connect( self.updateEditState_toBendLayer )
-        self.comboBox_pairsLayer.activated.connect( self.updateEditState_pairsLayer )
-        self.comboBox_pairsLayer.activated.connect( self.updateTransformationType )
-        self.restrictBox_pairsLayer.stateChanged.connect( self.updateTransformationType )
+        self.comboBox_toBendLayer.activated.connect(self.updateEditState_toBendLayer)
+        self.comboBox_pairsLayer.activated.connect(self.updateEditState_pairsLayer)
+        self.comboBox_pairsLayer.activated.connect(self.updateTransformationType)
+        self.restrictBox_pairsLayer.stateChanged.connect(self.updateTransformationType)
 
         # Create an event filter to update on focus
         self.installEventFilter(self)
@@ -57,14 +56,16 @@ class VectorBenderDialog(QtWidgets.QDialog):
         """
         Returns the current toBend layer depending on what is choosen in the comboBox_pairsLayer
         """
-        layerId = self.comboBox_toBendLayer.itemData(self.comboBox_toBendLayer.currentIndex())
-        return QgsProject.instance().mapLayer(layerId)
+        return QgsProject.instance().mapLayer(
+            self.comboBox_toBendLayer.itemData(self.comboBox_toBendLayer.currentIndex()))
+    
     def pairsLayer(self):
         """
         Returns the current pairsLayer layer depending on what is choosen in the comboBox_pairsLayer
         """
-        layerId = self.comboBox_pairsLayer.itemData(self.comboBox_pairsLayer.currentIndex())
-        return QgsProject.instance().mapLayer(layerId)
+        return QgsProject.instance().mapLayer(
+            self.comboBox_pairsLayer.itemData(self.comboBox_pairsLayer.currentIndex()))
+    
     def bufferValue(self):
         """
         Returns the current buffer value depending on the input in the spinbox
@@ -76,16 +77,14 @@ class VectorBenderDialog(QtWidgets.QDialog):
         """
         Updates the UI values, to be used upon opening / activating the window
         """
-
         # Update the comboboxes
         self.updateLayersComboboxes()
-
         # Update the edit mode buttons
         self.updateEditState_pairsLayer()
         self.updateEditState_toBendLayer()
-
         # Update the transformation type
         self.updateTransformationType()
+    
     def checkRequirements(self):
         """
         To be run after changes have been made to the UI. It enables/disables the run button and display some messages.
@@ -93,34 +92,27 @@ class VectorBenderDialog(QtWidgets.QDialog):
         # Checkin requirements
         self.runButton.setEnabled(False)
 
-        tbl = self.toBendLayer()
-        pl = self.pairsLayer()
+        to_bend_layer = self.toBendLayer()
+        pairs_layer = self.pairsLayer()
 
-        if tbl is None:
-            self.displayMsg( "You must select a vector layer to bend !", True )
-            return
-        if pl is None:
-            self.displayMsg( "You must select a vector (line) layer which defines the points pairs !", True )
-            return
-        if pl is tbl:
-            self.displayMsg( "The layer to bend must be different from the pairs layer !", True )
-            return            
-        if not tbl.isEditable():
-            self.displayMsg( "The layer to bend must be in edit mode !", True )
-            return
-        if not pl.isEditable() and self.pairsToPinsCheckBox.isChecked():
-            self.displayMsg( "The pairs layer must be in edit mode if you want to change pairs to pins !", True )
-            return
-        if self.stackedWidget.currentIndex() == 0:
+        if to_bend_layer is None:
+            self.displayMsg("You must select a vector layer to bend !", True )
+        elif pairs_layer is None:
+            self.displayMsg("You must select a vector (line) layer which defines the points pairs !", True )
+        elif pairs_layer is to_bend_layer:
+            self.displayMsg("The layer to bend must be different from the pairs layer !", True )
+        elif not to_bend_layer.isEditable():
+            self.displayMsg("The layer to bend must be in edit mode !", True )
+        elif not pairs_layer.isEditable() and self.pairsToPinsCheckBox.isChecked():
+            self.displayMsg("The pairs layer must be in edit mode if you want to change pairs to pins !", True )
+        elif self.stackedWidget.currentIndex() == 0:
             self.displayMsg("Impossible to run with an invalid transformation type.", True)
-            return            
-        self.displayMsg("Ready to go...")
-        self.runButton.setEnabled(True)
+        else:
+            self.displayMsg("Ready to go...")
+            self.runButton.setEnabled(True)
 
     def updateLayersComboboxes(self):
-        """
-        Recreate the comboboxes to display existing layers.
-        """
+        """ Recreate the comboboxes to display existing layers. """
         oldBendLayer = self.toBendLayer()
         oldPairsLayer = self.pairsLayer()
 
@@ -134,51 +126,55 @@ class VectorBenderDialog(QtWidgets.QDialog):
 
         if oldBendLayer is not None:
             index = self.comboBox_toBendLayer.findData(oldBendLayer.id())
-            self.comboBox_toBendLayer.setCurrentIndex( index )
+            self.comboBox_toBendLayer.setCurrentIndex(index)
         if oldPairsLayer is not None:
             index = self.comboBox_pairsLayer.findData(oldPairsLayer.id())
-            self.comboBox_pairsLayer.setCurrentIndex( index )
+            self.comboBox_pairsLayer.setCurrentIndex(index)
+    
     def updateEditState_pairsLayer(self):
-        """
-        Update the edit state button for pairsLayer
-        """
-        l = self.pairsLayer()
-        self.editModeButton_pairsLayer.setChecked( False if (l is None or not l.isEditable()) else True )
+        """ Update the edit state button for pairsLayer """
+        layer = self.pairsLayer()
+        self.editModeButton_pairsLayer.setChecked(
+            False if (layer is None or not layer.isEditable()) else True)
+        
     def updateEditState_toBendLayer(self):
-        """
-        Update the edit state button for toBendLayer
-        """
-        l = self.toBendLayer()
-        self.editModeButton_toBendLayer.setChecked( False if (l is None or not l.isEditable()) else True )
+        """ Update the edit state button for toBendLayer """
+        layer = self.toBendLayer()
+        self.editModeButton_toBendLayer.setChecked(
+            False if (layer is None or not layer.isEditable()) else True)
+        
     def updateTransformationType(self):
         """
         Update the stacked widget to display the proper transformation type. Also runs checkRequirements() 
         """
-        tt = self.vb.determineTransformationType()
-        self.stackedWidget.setCurrentIndex( tt )
+        self.stackedWidget.setCurrentIndex(self.vb.determineTransformationType())
 
         self.checkRequirements()
 
     # Togglers
     def toggleEditMode(self, checked, toBendLayer_True_pairsLayer_False):
-        l = self.toBendLayer() if toBendLayer_True_pairsLayer_False else self.pairsLayer()
-        if l is None:
+        layer = self.toBendLayer() if toBendLayer_True_pairsLayer_False else self.pairsLayer()
+        if layer is None:
             return 
 
         if checked:
-            l.startEditing()
+            layer.startEditing()
         else:
-            if not l.isModified():
-                l.rollBack()
+            if not layer.isModified():
+                layer.rollBack()
             else:
-                retval = QMessageBox.warning(self, "Stop editting", "Do you want to save the changes to layer %s ?" % l.name(), QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel, QMessageBox.Save)
+                return_value = QMessageBox.warning(
+                    self, "Stop editting", f"Do you want to save the changes to layer {layer.name()} ?",
+                    QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel, QMessageBox.Save)
 
-                if retval == QMessageBox.Save:
-                    l.commitChanges()
-                elif retval == QMessageBox.Discard:
-                    l.rollBack()
+                if return_value == QMessageBox.Save:
+                    layer.commitChanges()
+                elif return_value == QMessageBox.Discard:
+                    layer.rollBack()
+    
     def toggleEditMode_toBendLayer(self, checked):
         self.toggleEditMode(checked, True)
+    
     def toggleEditMode_pairsLayer(self, checked):
         self.toggleEditMode(checked, False)
 
@@ -188,14 +184,13 @@ class VectorBenderDialog(QtWidgets.QDialog):
         Creates a new memory layer to be used as pairLayer, and selects it in the ComboBox.
         """
 
-        suffix = ""
+        suffix = 0
         name = "Vector Bender"
-        while len( QgsProject.instance().mapLayersByName( name+suffix ) ) > 0:
-            if suffix == "": suffix = " 1"
-            else: suffix = " "+str(int(suffix)+1)
+        while len(QgsProject.instance().mapLayersByName(f"{name} {suffix}")) > 0:
+            suffix += 1
 
-        newMemoryLayer = QgsVectorLayer("Linestring", name+suffix, "memory")
-        newMemoryLayer.loadNamedStyle(os.path.join(os.path.dirname(__file__),'PairStyle.qml'), False)
+        newMemoryLayer = QgsVectorLayer("Linestring", f"{name} {suffix}", "memory")
+        newMemoryLayer.loadNamedStyle(join(dirname(__file__), "PairStyle.qml"), False)
         QgsProject.instance().addMapLayer(newMemoryLayer)
 
         self.updateLayersComboboxes()
@@ -203,68 +198,64 @@ class VectorBenderDialog(QtWidgets.QDialog):
         index = self.comboBox_pairsLayer.findData(newMemoryLayer.id())
         self.comboBox_pairsLayer.setCurrentIndex( index )
         
-        newMemoryLayer.startEditing()  
-    def displayMsg(self, msg, error=False):
-        if error:
-            #QApplication.beep()
-            msg = "<font color='red'>"+msg+"</font>"
-        self.statusLabel.setText( msg )  
+        newMemoryLayer.startEditing()
+        
+    def displayMsg(self, msg, error = False):
+        self.statusLabel.setText(
+            f"<font color='red'>{msg}</font>" if error else msg)
+    
     def hidePreview(self):
         if self.rubberBands is not None:
-            self.rubberBands[0].reset(QgsWkbTypes.PolygonGeometry)
-            self.rubberBands[1].reset(QgsWkbTypes.PolygonGeometry)
-            self.rubberBands[2].reset(QgsWkbTypes.PolygonGeometry)
+            for i in range(3):
+                self.rubberBands[i].reset(QgsWkbTypes.PolygonGeometry)
             self.rubberBands = None
+    
     def showPreview(self):
-
-        self.rubberBands = (QgsRubberBand(self.iface.mapCanvas(), QgsWkbTypes.PolygonGeometry),QgsRubberBand(self.iface.mapCanvas(), QgsWkbTypes.PolygonGeometry),QgsRubberBand(self.iface.mapCanvas(), QgsWkbTypes.PolygonGeometry))
-
-        self.rubberBands[0].reset(QgsWkbTypes.PolygonGeometry)
-        self.rubberBands[1].reset(QgsWkbTypes.PolygonGeometry)
-        self.rubberBands[2].reset(QgsWkbTypes.PolygonGeometry)
+        for _ in range(3):
+            rubberBands = QgsRubberBand(
+                self.iface.mapCanvas(), QgsWkbTypes.PolygonGeometry)
+            rubberBands.reset(QgsWkbTypes.PolygonGeometry)
+            self.rubberBands.append(rubberBands)
 
         pairsLayer = self.pairsLayer()
 
-        transformer = BendTransformer( pairsLayer, self.restrictBox_pairsLayer.isChecked() ,self.bufferValue() )
-
-        self.rubberBands[0].setColor(QColor(0,125,255))
-        self.rubberBands[1].setColor(QColor(255,125,0))
-        self.rubberBands[2].setColor(QColor(0,125,0,50))
-
-        self.rubberBands[0].setBrushStyle(Qt.Dense6Pattern)
-        self.rubberBands[1].setBrushStyle(Qt.Dense6Pattern)
-        self.rubberBands[2].setBrushStyle(Qt.NoBrush)
-
-        self.rubberBands[0].setWidth(3)
-        self.rubberBands[1].setWidth(3)
-        self.rubberBands[2].setWidth(1)
+        transformer = BendTransformer(
+            pairsLayer, self.restrictBox_pairsLayer.isChecked(), self.bufferValue())
+        
+        values = [
+            ((0, 125, 255), Qt.Dense6Pattern, 3),
+            ((255, 125, 0), Qt.Dense6Pattern, 3),
+            ((0, 125, 0, 50), Qt.NoBrush, 1)]
+        for n, value in enumerate(values):
+            self.rubberBands[n].setColor(QColor(*value[0]))
+            self.rubberBands[n].setBrushStyle(value[1])
+            self.rubberBands[n].setWidth(value[2])
       
-        #draw the expanded hull
+        # draw the expanded hull
         if transformer.expandedHull is not None:
-            for p in transformer.expandedHull.asPolygon()[0]:
-                self.rubberBands[0].addPoint( p, True, 0  )
-            for p in transformer.expandedHull.asPolygon()[0][0:1]:
+            for point in transformer.expandedHull.asPolygon()[0]:
+                self.rubberBands[0].addPoint(point, True, 0)
+            for point in transformer.expandedHull.asPolygon()[0][0:1]:
                 #we readd the first point since it's not possible to make true rings with rubberbands
-                self.rubberBands[0].addPoint( p, True, 0  )
-
-        #draw the hull
-        for p in transformer.hull.asPolygon()[0]:
-            self.rubberBands[0].addPoint( p, True, 0  ) #inner ring of rubberband 1
-            self.rubberBands[1].addPoint( p, True, 0  )
-        for p in transformer.hull.asPolygon()[0][0:1]:
-            #we readd the first point since it's not possible to make true rings with rubberbands
-            self.rubberBands[0].addPoint( p, True, 0  )
-
-        #draw the triangles
-        for i,tri in enumerate(transformer.delaunay.triangles):
-            self.rubberBands[2].addPoint( transformer.pointsA[tri[0]], False, i  )
-            self.rubberBands[2].addPoint( transformer.pointsA[tri[1]], False, i  )
-            self.rubberBands[2].addPoint( transformer.pointsA[tri[2]], True, i  ) #TODO : this refreshes the rubber band on each triangle, it should be updated only once after this loop       
+                self.rubberBands[0].addPoint(point, True, 0)
+        
+        # draw the hull
+        for point in transformer.hull.asPolygon()[0]:
+            self.rubberBands[0].addPoint(point, True, 0) # inner ring of rubberband 1
+            self.rubberBands[1].addPoint(point, True, 0)
+        for point in transformer.hull.asPolygon()[0][0:1]:
+            # we readd the first point since it's not possible to make true rings with rubberbands
+            self.rubberBands[0].addPoint(point, True, 0)
+        
+        # draw the triangles
+        for i, tri in enumerate(transformer.delaunay.triangles):
+            for j, boolean in enumerate([False, False, True]):
+                self.rubberBands[2].addPoint(transformer.pointsA[tri[j]], boolean, i)
+            #TODO : this refreshes the rubber band on each triangle, it should be updated only once after this loop
 
     # Events
-    def eventFilter(self,object,event):
+    def eventFilter(self, obj, event):
         if event.type() == QEvent.WindowActivate:
             self.refreshStates()
-        return False
 
 
